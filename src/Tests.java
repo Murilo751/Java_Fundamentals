2e21d598ca6ed8906f87658b5dce97d2e6dcf44f
Murilo Matos Aragão <murilo.matos2005@gmail.com>
2023-10-30 19:30:17 -0300

    ADD a package

--- src/Tests.java
+++
@@ -1,8 +0,0 @@
-//        Este arquivo é para testar codigos e aprendera utilizar as funões
-
-//        OBS: ao terminar de testar o codigo comente eletodo e de um titulo ao mesmo
-public class Tests {
-    public static void main(String[] args) {
-
-    }
-}
